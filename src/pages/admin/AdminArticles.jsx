15a53d8c15b55943b7bd1c97961f1fbe88e95d04
Usman Manu <52205926+vmafia@users.noreply.github.com>
2026-07-09 13:38:45 +0700

    Fix React is not defined in AdminArticles

--- src/pages/admin/AdminArticles.jsx
+++ src/pages/admin/AdminArticles.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useRef } from "react"
+import React, { useState, useEffect, useMemo, useRef } from "react"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
 
